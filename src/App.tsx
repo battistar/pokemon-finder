6d088f9a8df4aceaa2b64e890839e09fd927b16a
Samuele Battistella <battistar92@gmail.com>
2023-03-31 17:00:23 +0200

    Add search

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,29 @@
 import Card from 'components/Card';
 import { PokemonProvider, usePokemon } from 'providers/store';
+import { ChangeEvent } from 'react';
+
+const Search = (): JSX.Element => {
+  const { search, setSearch } = usePokemon();
+
+  const handleChange = (event: ChangeEvent<HTMLInputElement>): void => {
+    const text = event.target.value;
+
+    setSearch(text);
+  };
+
+  return (
+    <div className="search">
+      <input
+        className="search--input"
+        name="search"
+        type="text"
+        placeholder="Search Pokemon..."
+        onChange={handleChange}
+        value={search}
+      />
+    </div>
+  );
+};
 
 const PokemonList = (): JSX.Element => {
   const { pokemon } = usePokemon();
@@ -23,6 +47,7 @@ const App = (): JSX.Element => {
   return (
     <PokemonProvider>
       <main>
+        <Search />
         <PokemonList />
       </main>
     </PokemonProvider>
