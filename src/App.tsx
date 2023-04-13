d8f0867e2c30ff0a97b9f37531efa84bc2895256
Samuele Battistella <battistar92@gmail.com>
2023-04-13 11:42:19 +0200

    Update filters mobile compatibility

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,12 @@ import DualRangeSlider from 'components/DualRangeSlider';
 import FilterChip from 'components/FilterChip';
 import Search from 'components/Search';
 import { PokemonProvider, Type, attributes, types, usePokemon } from 'providers/store';
-import { useCallback } from 'react';
+import { useCallback, useState } from 'react';
 
 const Filters = (): JSX.Element => {
   const { filters, setSearch, setType, setHP, setAttack, setDefense, setSpecialAttack, setSpecialDefense, setSpeed } =
     usePokemon();
+  const [toggle, setToggle] = useState(false);
 
   const handleClickChip = useCallback(
     (type: Type): void => {
@@ -65,64 +66,75 @@ const Filters = (): JSX.Element => {
     [setSpeed]
   );
 
+  const handleToggle = (): void => {
+    setToggle((prevToggle) => {
+      return !prevToggle;
+    });
+  };
+
   return (
     <div className="filter--container">
       <Search value={filters.search} onChange={handleChangeSearch} />
-      <div className="filter--types-container">
-        {types.map((type) => {
-          return (
-            <FilterChip
-              key={type}
-              type={type as Type}
-              onClick={handleClickChip}
-              active={filters.types.includes(type as Type)}
-            />
-          );
-        })}
-      </div>
-      <div className="filter--range-container">
-        <DualRangeSlider
-          title="HP"
-          min={attributes.hp.min}
-          max={attributes.hp.max}
-          value={filters.hp}
-          onChange={handleHPChange}
-        />
-        <DualRangeSlider
-          title="Attack"
-          min={attributes.attack.min}
-          max={attributes.attack.max}
-          value={filters.attack}
-          onChange={handleAttackChange}
-        />
-        <DualRangeSlider
-          title="Defense"
-          min={attributes.defense.min}
-          max={attributes.defense.max}
-          value={filters.defense}
-          onChange={handleDefenseChange}
-        />
-        <DualRangeSlider
-          title="Special Attack"
-          min={attributes.specialAttack.min}
-          max={attributes.specialAttack.max}
-          value={filters.specialAttack}
-          onChange={handleSpecialAttackChange}
-        />
-        <DualRangeSlider
-          title="Special Defense"
-          min={attributes.specialDefense.min}
-          max={attributes.specialDefense.max}
-          value={filters.specialDefense}
-          onChange={handleSpecialDefenseChange}
-        />
-        <DualRangeSlider
-          title="Speed"
-          min={attributes.speed.min}
-          max={attributes.speed.max}
-          value={filters.speed}
-          onChange={handleSpeedChange}
-        />
+      <button className="filter-advanced--toggle" onClick={handleToggle}>
+        {toggle ? 'Hide filters ↑' : 'Show filters ↓'}
+      </button>
+      <div className={`filter-advanced--container ${!toggle && 'hide'}`}>
+        <div className="filter-advanced--types-container">
+          {types.map((type) => {
+            return (
+              <FilterChip
+                key={type}
+                type={type as Type}
+                onClick={handleClickChip}
+                active={filters.types.includes(type as Type)}
+              />
+            );
+          })}
+        </div>
+        <div className="filter-advanced--range-container">
+          <DualRangeSlider
+            title="HP"
+            min={attributes.hp.min}
+            max={attributes.hp.max}
+            value={filters.hp}
+            onChange={handleHPChange}
+          />
+          <DualRangeSlider
+            title="Attack"
+            min={attributes.attack.min}
+            max={attributes.attack.max}
+            value={filters.attack}
+            onChange={handleAttackChange}
+          />
+          <DualRangeSlider
+            title="Defense"
+            min={attributes.defense.min}
+            max={attributes.defense.max}
+            value={filters.defense}
+            onChange={handleDefenseChange}
+          />
+          <DualRangeSlider
+            title="Special Attack"
+            min={attributes.specialAttack.min}
+            max={attributes.specialAttack.max}
+            value={filters.specialAttack}
+            onChange={handleSpecialAttackChange}
+          />
+          <DualRangeSlider
+            title="Special Defense"
+            min={attributes.specialDefense.min}
+            max={attributes.specialDefense.max}
+            value={filters.specialDefense}
+            onChange={handleSpecialDefenseChange}
+          />
+          <DualRangeSlider
+            title="Speed"
+            min={attributes.speed.min}
+            max={attributes.speed.max}
+            value={filters.speed}
+            onChange={handleSpeedChange}
+          />
+        </div>
       </div>
     </div>
   );
